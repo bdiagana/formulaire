c0bd5abd19eb82c94e2e6225ead17ceee234a3a3
Arthur DESCHAUD <arthur.deschaud@edu.itescia.fr>
2019-05-23 10:28:39 +0200

    suppression de prise en charge de plusieurs fichiers de conf

--- load_conf.js
+++ load_conf.js
@@ -2,14 +2,9 @@ const ini = require('ini');
 const fs = require('fs');
 
 function parse_ini(){
-    var file = fs.readFileSync('./conf/global-app.conf','utf8', (err) => {
+    var file = fs.readFileSync('./conf/node-app.conf','utf8', (err) => {
         if (err) throw err;
     });
-    if (!file){
-    	file = fs.readFileSync('./conf/node-app.sample.conf','utf8', (err) => {
-        	if (err) throw err;
-    	});
-    }
     return ini.parse(file);
 }
 
