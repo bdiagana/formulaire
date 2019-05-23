d01a02d5e51227e6d1544a39b9189a9126fc1298
Arthur DESCHAUD <arthur.deschaud@edu.itescia.fr>
2019-05-23 12:12:55 +0200

    modification des headers inclusion css

--- formulaires/header.php
+++ formulaires/header.php
@@ -3,15 +3,9 @@
 	<head> 
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-		<link type="text/css" href="css/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-		<link type="text/css" href="css/font-awesome/css/font-awesome.css" rel="stylesheet">
-		<link type="text/css" href="css/datepicker/css/bootstrap-datepicker.css" rel="stylesheet">
-		<link type="text/css" href="css/chosen/css/chosen.css" rel="stylesheet">
-		<link type="text/css" href="css/select2/css/select2.min.css" rel="stylesheet">
-		<link type="text/css" href="css/select2/css/select2-bootstrap.css" rel="stylesheet">
-		<link type="text/css" href="css/jqtree/jqtree.css" rel="stylesheet">
-		<link type="text/css" href="css/application.css" rel="stylesheet">
-		<link type="text/css" href="/css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link type="text/css" href="css/bootstrap.css" rel="stylesheet">
+		<link type="text/css" href="css/bootstrap-reboot.css" rel="stylesheet">
+		<link type="text/css" href="css/bootstrap-grid.css" rel="stylesheet">
 		<title>GEDi: Connexion</title>
 	</head>
 
